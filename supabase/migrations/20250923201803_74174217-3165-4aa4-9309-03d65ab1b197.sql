41aba828a0e49379ddc55632549e22c8f6b07905
lehan0328 <lehanouyang@gmail.com>
2025-11-17 19:14:07 -0800

    add space

--- supabase/migrations/20250923201803_74174217-3165-4aa4-9309-03d65ab1b197.sql
+++ supabase/migrations/20250923201803_74174217-3165-4aa4-9309-03d65ab1b197.sql
@@ -65,3 +65,4 @@ $$ LANGUAGE plpgsql SECURITY DEFINER SET search_path = public;
 CREATE TRIGGER on_auth_user_created
   AFTER INSERT ON auth.users
   FOR EACH ROW EXECUTE FUNCTION public.handle_new_user();
+  
